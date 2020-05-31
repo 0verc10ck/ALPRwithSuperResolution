d1667ec2e3a5f701662e924d3239be6ffdcd2ce4
0verc10ck <31722109+0verc10ck@users.noreply.github.com>
2020-06-01 02:38:15 +0900

    merged SR
    merged SR to main

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from ctypes import *
+from PIL import Image
+from torchvision.models import vgg19
+from models import GeneratorRRDB
+from torch.autograd import Variable
+from torchvision import transforms
+from torchvision.utils import save_image
+from datasets import denormalize, mean, std
+
 import math
 import random
 import os
 import cv2
 import numpy as np
 import darknet
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch
-from torchvision.models import vgg19
-
+import argparse
+import pytesseract
 
 netMain = None
 metaMain = None
@@ -64,13 +72,15 @@ def Detector():
             pass
 
     darknet_image = darknet.make_image(darknet.network_width(netMain), darknet.network_height(netMain),3)
+   
     for i in range(0, len(list_img)):  
         path = './data/testn/' + list_img[i]
         detections = (darknet.performDetect(path, 0.25, configPath, weightPath, metaPath, False, False))
-        preprocess(path, detections)
+        SuperResolution(list_img[i], preprocess(path, detections))
 
      
         
+#center_x, center_y, w, h
 def preprocess(path, detections):
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     f_name = path.split('/')[-1]
@@ -98,17 +108,55 @@ def preprocess(path, detections):
     
     matrix = cv2.getPerspectiveTransform(coord, dst2)
     img_t = cv2.warpPerspective(img, matrix, (350, 100))
-    
+    del img
     #save
     transpath = "./data/preprocessed_img"
     try:
-        cv2.imwrite(os.path.join(transpath,f_name),img_t)
+        cv2.imwrite(os.path.join(transpath,f_name), img_t)
     
     except ValueError:
         print("Cannot save file ", f_name, "\n")
         pass
     
+    return img_t
+
+
+def PSNR(img1, img2):
+    mse = np.mean(( img1 - img2) ** 2)
+    print("mse : ", mse)
+    if mse == 0:
+        return 100
+    PIXEL_MAX = 255.0
+    return 20* math.log10(PIXEL_MAX / math.sqrt(mse))
+
+def SuperResolution(f_name, ori):
+    pth = "./generator.pth"
+    channels = 3
+    residual_blocks = 23
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    
+    # Define model and load model checkpoint
+    generator = GeneratorRRDB(channels, filters=64, num_res_blocks=residual_blocks).to(device)
+    generator.load_state_dict(torch.load(pth))
+    generator.eval()
+
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean, std)])
+
+    # Prepare input
+    
+    image_tensor = Variable(transform(ori)).to(device).unsqueeze(0)
+
+    # Upsample image
+
+    with torch.no_grad():
+        sr_image = denormalize(generator(image_tensor)).cpu()
+
+    # Save image
+    path = os.path.join("./data/sr_img/",f_name)
+    save_image(sr_image, path)
+    #OCR(path)
+    
+
 if __name__ == "__main__":
     init()
     Detector()
-
